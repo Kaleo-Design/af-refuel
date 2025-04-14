52305a3a9c3bf54851c735c26beba493f9c2c22b
SamIAmHarris <samharris512@gmail.com>
2025-04-14 13:51:47 -0400

    Add custom code on Android side to get permissions.

--- android/app/src/main/kotlin/com/example/my_project/HealthDataPlugin.kt
+++ android/app/src/main/kotlin/com/example/my_project/HealthDataPlugin.kt
@@ -1,11 +1,29 @@
 package com.example.my_project
 
+import android.app.Activity
+import androidx.core.os.BuildCompat
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.HealthConnectClient.Companion.SDK_AVAILABLE
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.embedding.android.FlutterActivity
 
+@OptIn(BuildCompat.PrereleaseSdkCheck::class)
+class HealthDataPlugin(private val activity: Activity) : MethodChannel.MethodCallHandler {
 
-class HealthDataPlugin(private val activity: FlutterActivity) : MethodChannel.MethodCallHandler {
+    private val healthConnectClient by lazy { HealthConnectClient.getOrCreate(activity.baseContext) }
+
+    private var availability = HealthConnectAvailability.NOT_SUPPORTED
+
+    init {
+        checkAvailability()
+    }
+
+    private fun checkAvailability() {
+        availability = when {
+            HealthConnectClient.getSdkStatus(activity.baseContext) == SDK_AVAILABLE -> HealthConnectAvailability.INSTALLED
+            else -> HealthConnectAvailability.NOT_INSTALLED
+        }
+    }
 
     override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
         when (call.method) {
@@ -41,4 +59,10 @@ class HealthDataPlugin(private val activity: FlutterActivity) : MethodChannel.Me
 
     private fun addTestSleepData(result: MethodChannel.Result) { /* ... */
     }
+}
+
+enum class HealthConnectAvailability {
+    INSTALLED,
+    NOT_INSTALLED,
+    NOT_SUPPORTED
 }
--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -1,11 +1,69 @@
 package com.hope4you.refuel.af
 
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.os.BuildCompat
+import androidx.health.connect.client.HealthConnectClient
+import androidx.health.connect.client.PermissionController
+import androidx.health.connect.client.permission.HealthPermission
+import androidx.health.connect.client.records.DistanceRecord
+import androidx.health.connect.client.records.ExerciseSessionRecord
+import androidx.health.connect.client.records.HeartRateRecord
+import androidx.health.connect.client.records.SleepSessionRecord
+import androidx.health.connect.client.records.StepsRecord
 import com.example.my_project.HealthDataPlugin
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+@OptIn(BuildCompat.PrereleaseSdkCheck::class)
+class MainActivity : FlutterFragmentActivity() {
+
+    private val PERMISSIONS_CHANNEL = "health_connect_permissions"
+
+    private var pendingResult: MethodChannel.Result? = null
+
+    private val healthConnectClient by lazy { HealthConnectClient.getOrCreate(context = this.baseContext) }
+
+    private val permissions = setOf(
+        HealthPermission.getReadPermission(ExerciseSessionRecord::class),
+        HealthPermission.getReadPermission(SleepSessionRecord::class),
+        HealthPermission.getReadPermission(StepsRecord::class),
+        HealthPermission.getReadPermission(DistanceRecord::class),
+        HealthPermission.getReadPermission(HeartRateRecord::class)
+    )
+
+    private lateinit var permissionLauncher: ActivityResultLauncher<Set<String>>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Register the result launcher using the new contract
+        permissionLauncher = registerForActivityResult(
+            PermissionController.createRequestPermissionResultContract()
+        ) { grantedPermissions ->
+            val granted = permissions.all { it in grantedPermissions }
+            pendingResult?.success(granted)
+            pendingResult = null
+        }
+
+        MethodChannel(
+            flutterEngine!!.dartExecutor.binaryMessenger,
+            PERMISSIONS_CHANNEL
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "requestPermissions" -> {
+                    permissionLauncher.launch(permissions)
+                    pendingResult = result
+                }
+
+                else -> {
+                    pendingResult = result
+                    pendingResult?.notImplemented()
+                }
+            }
+        }
+    }
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
@@ -17,5 +75,10 @@ class MainActivity: FlutterActivity() {
 
         val plugin = HealthDataPlugin(this)
         channel.setMethodCallHandler(plugin)
+    }
+
+    suspend fun hasAllPermissions(permissions: Set<String>): Boolean {
+        return healthConnectClient.permissionController.getGrantedPermissions()
+            .containsAll(permissions)
     }
 }
